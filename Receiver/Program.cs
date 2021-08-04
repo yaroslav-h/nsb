c7f365d60889682cfd295d39a7fdbfac97af3f89
Yaroslav Hryn <yaroslav.hryn@itera.no>
2021-08-04 10:13:57 +0300

    Initial commit

---
+++ Receiver/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+using System.IO;
+using System.Threading.Tasks;
+using NServiceBus;
+
+public static class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.Sql.Receiver";
+
+        #region ReceiverConfiguration
+
+        var endpointConfiguration = new EndpointConfiguration("Samples.Sql.Receiver");
+        endpointConfiguration.SendFailedMessagesTo("error");
+        endpointConfiguration.AuditProcessedMessagesTo("audit");
+        endpointConfiguration.EnableInstallers();
+        var connection = @"Data Source=192.168.21.214,1433;Database=NsbSamplesSql;User Id=sa;Password=Lineoftd1;Max Pool Size=100";
+
+        var transport = endpointConfiguration.UseTransport<SqlServerTransport>();
+        transport.ConnectionString(connection);
+        transport.DefaultSchema("receiver");
+        transport.UseSchemaForQueue("error", "dbo");
+        transport.UseSchemaForQueue("audit", "dbo");
+        transport.UseSchemaForQueue("Samples.Sql.Sender", "sender");
+        transport.Transactions(TransportTransactionMode.SendsAtomicWithReceive);
+
+        var subscriptions = transport.SubscriptionSettings();
+        subscriptions.CacheSubscriptionInformationFor(TimeSpan.FromMinutes(1));
+        subscriptions.SubscriptionTableName(tableName: "Subscriptions", schemaName: "dbo");
+
+        var routing = transport.Routing();
+        routing.RouteToEndpoint(typeof(OrderAccepted), "Samples.Sql.Sender");
+
+        var persistence = endpointConfiguration.UsePersistence<SqlPersistence>();
+        var dialect = persistence.SqlDialect<SqlDialect.MsSqlServer>();
+        dialect.Schema("receiver");
+        persistence.ConnectionBuilder(() => new SqlConnection(connection));
+        persistence.TablePrefix("");
+
+        #endregion
+
+        SqlHelper.CreateSchema(connection, "receiver");
+        var allText = File.ReadAllText("Startup.sql");
+        SqlHelper.ExecuteSql(connection, allText);
+        var endpointInstance = await Endpoint.Start(endpointConfiguration).ConfigureAwait(false);
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop().ConfigureAwait(false);
+    }
+}
