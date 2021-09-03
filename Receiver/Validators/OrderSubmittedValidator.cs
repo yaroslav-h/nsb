942579633139a1880cd5badc9cdc981404669a0c
Yaroslav Hryn <yaroslav.hryn@itera.no>
2021-09-03 14:47:21 +0300

    Add OrderSubmittedValidator

---
+++ Receiver/Validators/OrderSubmittedValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Receiver.Validators
+{
+    public class OrderSubmittedValidator : AbstractValidator<OrderSubmitted>
+    {
+        public OrderSubmittedValidator()
+        {
+            RuleFor(x => x.Value).GreaterThan(1);
+        }
+    }
+}
